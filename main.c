66f1943487933c25f98322e7cd78c350643f2cb9
kxigor <garik.irk@mail.ru>
2024-07-05 23:18:52 +0300

    It's being written slowly

--- main.c
+++ main.c
@@ -16,12 +16,13 @@
         putchar('\n');                          \
     }                                           \
     while(0)                                    \
-extern void outllll();
+
 int main(void)
 {
     uint64_t arr[] = {
         0, 9, 8, 7, 6, 5, 4, 3, 2, 1, 2, 3, 4, 5, 4, 3, 2, 1, 5, 6, 7, 6, 5, 4, 3, 2, 1, 8, 9, 0
     };
-
+    printf("%p\n", arr);
     radix_asm_uint64(arr, sizearr(arr));
+    printf("Exit the program succes...\n");
 }
--- src/radix_asm_uint64/radix_asm_uint64.asm
+++ src/radix_asm_uint64/radix_asm_uint64.asm
@@ -1,6 +1,78 @@
 global radix_asm_uint64
+extern printf
+extern calloc
+extern free
 
 section .text
+
+;-------------------------
+;
+; RADIX ASM UINT64 SORT
+;
+; void radix_asm_uint64(arr[], arrSize)
+; rdi = uint64_t arr[]
+; rsi = size_t arrSize
+;
+;-------------------------
 radix_asm_uint64:
-    ret
+
+    ;push rsi                   ;
+    ;xor rax, rax               ;
+    ;mov rsi, rdi               ; printf("%p\n", rdi);
+    ;mov rdi, spec_p            ;
+    ;call printf WRT ..plt      ;
+
+    ;pop rsi                    ;
+    ;xor rax, rax               ;
+    ;mov rsi, rsi               ; printf("%lu\n", rsi);
+    ;mov rdi, spec_lu           ;
+    ;call printf WRT ..plt      ;
+
+    test rdi, rdi   ; if(rdi == NULL)
+    jz RAU64_EXIT   ;   return;
+
+    test rsi, rsi   ; if(rsi == NULL)
+    jz RAU64_EXIT   ;   return;
+
+    push rdi
+    push rsi
+
+    mov rdi, rsi            ; rdi = arrSize
+    mov rsi, 0x8            ; rsi = 0x8 = sizeof(uint64_t)
+    call calloc WRT ..plt   ; tmp = rax = calloc(rdi, rsi)
+
+    test rax, rax           ; if(rax == NULL)
+    jz RAU64_EXIT           ;   return;
+    push rax
+
+    ;pop rdi
+    ;pop rsi
+
+    xor rcx, rcx
+    RAU64_BYTE_LOOP:    ; for(size_t rcx = 0; rcx < sizeof(uint64_t); rcx++) {
+
+        push rcx                    ;
+        mov rcx, 257                ; 257 = sizeof(ctr)
+        mov rax, ctr                ;
+        RAU64_MEMSET_LOOP:          ;
+            mov qword [rax], 0x0    ; memset(ctr, 0, sizeof(ctr));
+            add rax, 0x8            ; 0x8 = sizeof(size_t)
+        loop RAU64_MEMSET_LOOP      ;
+        pop rcx                     ;
+
+
+    inc rcx             ;
+    cmp rcx, 0x8        ; 0x8 = sizeof(uint64_t)
+    jb RAU64_BYTE_LOOP  ; }
+
+
+    ;mov rdi, rax            ; rdi = rax = tmp
+    call free WRT ..plt     ; free(rdi)
+
+    RAU64_EXIT:
+    ret     ; return
+
 section .data
+    spec_p  : db "%p",  10, 0   ; specifier for pointer
+    spec_lu : db "%lu", 10, 0   ; specifier for size_t
+    ctr     : times 257 dq 0    ; 257 = MAX_BYTE_SIZE + 1
